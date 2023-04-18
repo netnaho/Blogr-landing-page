4279cdfb0e6f53e8fe2f9227b54470f8d124dab3
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-17 23:33:15 -0800

    added ham-menu functionality

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-const dropdown = document.querySelectorAll("i");
-const links = document.querySelectorAll(".nav-links");
+const chevron = document.querySelectorAll(".prod");
+const hamMenu = document.querySelector(".ham-menu");
+const mobNav = document.querySelector(".mob-nav");
 
-for (i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function (e) {
-    let target = e.target;
-    console.log(target.firstElement.nextElement.nextElement);
+hamMenu.addEventListener("click", function () {
+  this.classList.toggle("active");
+  mobNav.classList.toggle("hidden");
+});
+// window.addEventListener("click", function (e) {
+//   if (e.target != hamMenu) {
+//     mobNav.classList.add("hidden");
+//     hamMenu.classList.toggle("active");
+//   }
+// });
+for (i = 0; i < chevron.length; i++) {
+  chevron[i].addEventListener("click", function (e) {
+    this.classList.toggle("active");
+    if (!this.nextElementSibling.classList.contains("fa-chevron-down")) {
+      this.nextElementSibling.classList.add("fa-chevron-down");
+      this.nextElementSibling.classList.remove("fa-chevron-up");
+    } else {
+      this.nextElementSibling.classList.add("fa-chevron-up");
+      this.nextElementSibling.classList.remove("fa-chevron-down");
+    }
+    this.nextElementSibling.style.transition = "all 2s";
   });
 }
